4d56b31844f389973c89205cd488d7dac2eb446a
kayacchang <egg734631@gmail.com>
2021-05-21 18:02:53 +0800

    add color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          dark: "#00628F",
+        },
+        green: {
+          dark: "#57746E",
+        },
+        gold: {
+          light: "#FFE074",
+          dark: "#C0A236",
+        },
+        gray: {
+          DEFAULT: "#747474",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
