871364545e598d366e63d3239e40c0a351d1f4ef
kayacchang <egg734631@gmail.com>
2021-05-28 16:42:35 +0800

    fix menu button style

--- components/molecules/Menu.tsx
+++ components/molecules/Menu.tsx
@@ -17,10 +17,10 @@ type ButtonProps = {
 function Button({ open }: ButtonProps) {
   return (
     <Disclosure.Button
-      className="lg:hidden bg-green-dark text-white px-5 rounded-bl-2xl"
+      className="lg:hidden bg-green-dark text-white px-5 rounded-bl-3xl"
       aria-label={open ? "Close Menu" : "Open Menu"}
     >
-      <span className="block w-5">
+      <span className="block w-6">
         <Icon.Menu />
       </span>
     </Disclosure.Button>
---
+++ pages/client/dispatch.tsx
@@ -0,0 +1,5 @@
+import Layout from "components/templates";
+
+export default function News() {
+  return <Layout.Normal title="預約訂車"></Layout.Normal>;
+}
