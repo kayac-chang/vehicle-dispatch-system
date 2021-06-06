278dbb5fc5f05e3bd1ec919dc0ee6ddfd5aa6d7e
kayacchang <egg734631@gmail.com>
2021-06-06 15:12:04 +0800

    fix button font size

--- components/dispatch/RouteMap.tsx
+++ components/dispatch/RouteMap.tsx
@@ -11,14 +11,14 @@ export function RouteMap() {
 
   return (
     <div className="-mx-4">
-      <div className="bg-black bg-opacity-75 flex justify-end text-xs py-3 space-x-4 px-4">
-        <div className="w-1/3">
-          <Button.Outline type="button" className="bg-white h-full">
+      <div className="bg-black bg-opacity-75 flex justify-end text-xs lg:text-sm py-3 space-x-4 px-4">
+        <div className="w-1/3 lg:w-auto">
+          <Button.Outline type="button" className="bg-white h-full p-2">
             新增下個地點
           </Button.Outline>
         </div>
-        <div className="w-1/3">
-          <Button.Flat type="button" className="h-full py-2">
+        <div className="w-1/3 lg:w-auto">
+          <Button.Flat type="button" className="h-full p-2">
             立即預約
           </Button.Flat>
         </div>
