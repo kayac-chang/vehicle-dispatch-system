5a0164d20ef8e41a83e916c4b3e2e9e9bf982e0e
kayacchang <egg734631@gmail.com>
2021-06-06 15:50:25 +0800

    refactor qna

--- pages/client/qna.tsx
+++ pages/client/qna.tsx
@@ -3,67 +3,40 @@ import clsx from "clsx";
 import Layout from "components/templates";
 import { Icon } from "components/atoms";
 import { Accordion } from "components/molecules";
+import path from "path";
+import { promises as fs } from "fs";
+import { InferGetStaticPropsType as Infer } from "next";
 
-const data = [
-  {
-    name: `panel-1`,
-    title: "在交通平台系統最早可以預約訂車期間為何?",
-    description: `
-        預約時間10分鐘後若司機未抵達上車地點，該訂單上會顯示「司機未執行」，民眾可觸發此按鈕，即可將訂單取消，並歸還補助額度。也請通知您所屬個案管理師知曉。?
-    `,
-  },
-  {
-    name: `panel-2`,
-    title: "在交通平台系統最早可以預約訂車期間為何?1",
-    description: `
-        預約時間10分鐘後若司機未抵達上車地點，該訂單上會顯示「司機未執行」，民眾可觸發此按鈕，即可將訂單取消，並歸還補助額度。也請通知您所屬個案管理師知曉。?
-    `,
-  },
-  {
-    name: `panel-3`,
-    title: "在交通平台系統最早可以預約訂車期間為何?2",
-    description: `
-        預約時間10分鐘後若司機未抵達上車地點，該訂單上會顯示「司機未執行」，民眾可觸發此按鈕，即可將訂單取消，並歸還補助額度。也請通知您所屬個案管理師知曉。?
-    `,
-  },
-  {
-    name: `panel-4`,
-    title: "在交通平台系統最早可以預約訂車期間為何?3",
-    description: `
-        預約時間10分鐘後若司機未抵達上車地點，該訂單上會顯示「司機未執行」，民眾可觸發此按鈕，即可將訂單取消，並歸還補助額度。也請通知您所屬個案管理師知曉。?
-    `,
-  },
-  {
-    name: `panel-5`,
-    title: "在交通平台系統最早可以預約訂車期間為何?4",
-    description: `
-        預約時間10分鐘後若司機未抵達上車地點，該訂單上會顯示「司機未執行」，民眾可觸發此按鈕，即可將訂單取消，並歸還補助額度。也請通知您所屬個案管理師知曉。?
-    `,
-  },
-  {
-    name: `panel-6`,
-    title: "在交通平台系統最早可以預約訂車期間為何?5",
-    description: `
-        預約時間10分鐘後若司機未抵達上車地點，該訂單上會顯示「司機未執行」，民眾可觸發此按鈕，即可將訂單取消，並歸還補助額度。也請通知您所屬個案管理師知曉。?
-    `,
-  },
-  {
-    name: `panel-7`,
-    title: "在交通平台系統最早可以預約訂車期間為何?6",
-    description: `
-        預約時間10分鐘後若司機未抵達上車地點，該訂單上會顯示「司機未執行」，民眾可觸發此按鈕，即可將訂單取消，並歸還補助額度。也請通知您所屬個案管理師知曉。?
-    `,
-  },
-];
+interface Post {
+  name: string;
+  title: string;
+  description: string;
+}
 
-export default function News() {
-  const [expanded, setExpanded] = useState(data[0].name);
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "static", "qna.json");
+  const file = await fs.readFile(filePath, "utf-8");
+
+  return {
+    props: {
+      posts: JSON.parse(file) as Post[],
+    },
+  };
+}
+
+const content = {
+  title: "常見問題",
+};
+
+type Props = Infer<typeof getStaticProps>;
+export default function QNA({ posts }: Props) {
+  const [expanded, setExpanded] = useState(posts[0].name);
 
   return (
-    <Layout.Normal title="常見問題">
+    <Layout.Normal title={content.title}>
       <div className="-mx-6 lg:mx-0 pb-8">
         <ul>
-          {data.map(({ name, title, description }) => (
+          {posts.map(({ name, title, description }) => (
             <li
               key={title}
               className={clsx(
