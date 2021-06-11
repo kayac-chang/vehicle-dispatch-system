3992410f5f5372dfe370970f5a1a1b0fc32629ee
kayacchang <egg734631@gmail.com>
2021-06-11 22:18:47 +0800

    add css env support

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    "postcss-import",
+    "tailwindcss",
+    ["postcss-preset-env", { stage: 1 }],
+  ],
+};
